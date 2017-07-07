0c93c40b8b041067e0b0899a0744c80ae2c876ed
unique-pear486 <unique-pear486@users.noreply.github.com>
2017-07-07 19:57:41 +1000

    Add virtualenvwapper to install packages

--- .bashrc
+++ .bashrc
@@ -8,3 +8,11 @@ alias refalac='wine ~/.wine/drive_c/qaac/refalac.exe'
 # Turn off a few pesky warnings (well, actually all of them!):
 export WINEDEBUG=-all
 export PATH=$PATH:~/bin/
+
+# Load virtualenvwrapper for python
+venvwrap="virtualenvwrapper.sh"
+/usr/bin/which $venvwrap > /dev/null
+if [ $? -eq 0 ]; then
+    venvwrap=`/usr/bin/which $venvwrap`
+    source $venvwrap
+fi
--- packages.sh
+++ packages.sh
@@ -15,41 +15,5 @@ do
     apt-get install -y $package
 done
 
-# Get google app engine if not already installed
-if ! [ -d "/opt/google_appengine" ]; then
-    echo "installing Google App Engine v1.9.30"
-    wget -O /tmp/GAE.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.40.zip
-    unzip -q /tmp/GAE.zip -d /opt/
-    rm /tmp/GAE.zip
-    if ! grep -q "google app engine" ~/.profile ; then
-        echo "Adding GAE to .profile"
-        echo "" >> ~/.profile
-        echo "# Installed google app engine" >> ~/.profile
-        echo "export PATH=\$PATH:/opt/google_appengine" >> ~/.profile
-        echo "export PYTHONPATH=\$PYTHONPATH:/opt/google_appengine" >> ~/.profile
-    fi
-fi
-
-# Get node if not already installed
-if ! [ -d "/opt/node" ]; then
-    echo "installing node.js v0.12.7 x64"
-    wget -O /tmp/node.tar.gz https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x64.tar.gz
-    tar xzf /tmp/node.tar.gz -C /opt/
-    mv /opt/node-v0.12.7-linux-x64 /opt/node
-    rm /tmp/node.tar.gz
-    if ! grep -q "node.js" ~/.profile ; then
-        echo "Adding node.js to .profile"
-        echo "" >> ~/.profile
-        echo "# Installed node.js and eslint" >> ~/.profile
-        echo "export PATH=\$PATH:/opt/node/bin" >> ~/.profile
-    fi
-fi
-
-export PATH=$PATH:/opt/node/bin
-# Get node packages
-for package in eslint
-do
-    if ! npm list -g $package ; then
-        npm install -g $package
-    fi
-done
+# install python packages
+pip install virtualenvwrapper
